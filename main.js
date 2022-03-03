14f3f91f96ef82c48746261bac5c7e563a53f090
Gerardo <gpadil@icloud.com>
2022-03-02 17:18:00 -0700

    final

--- main.js
+++ main.js
@@ -7,14 +7,15 @@ function getButton () {
     axios.get('https://swapi.dev/api/planets?search=Alderaan')
         .then(response => {
             // console.log(response.data)
-            let resArray = response.data['results'][0]['residents']
+            let resArray = response.data.results[0].residents
                 
             for (let i = 0; i < resArray.length; i++) {
-                    axios.get(resArray[i]).then(resident => {
+                    axios.get(resArray[i])
+                        .then(resident => {
                         
                         let h2 = document.createElement('h2');
                             // console.log(resident.data)
-                        h2.textContent = resident.data['name']
+                        h2.textContent = resident.data.name
 
                         document.body.appendChild(h2);
 
